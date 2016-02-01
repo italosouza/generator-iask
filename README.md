18c1cad3ad9cc07313e33ca6289f7d8b1060fff7
italosouza <italoasouza@gmail.com>
2016-02-01 12:26:46 -0200

    alterado o teste basico

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
-# IASK
+# IASK [![Build Status](https://travis-ci.org/italosouza/generator-iask.svg?branch=master)](https://travis-ci.org/italosouza/generator-iask) 
+
+> [Yeoman](http://yeoman.io) generator para prototipação de aplicações web utilizando [gulp](http://gulpjs.com/) como ferramenta de construção.
 
 ## Passo a passo
 
-- Instale as dependencias: `npm install --g yo gulp bower`
-- instale o gerador: `npm install --g generator-iask`
-- Execute `yo iask` para criar o projeto
-- Execute `yo iask:scaffold modulo` para criar um módulo
-- Execute `gulp build` para construir a versão de produção do projeto 
+- Instale as dependencias: `npm install -g yo gulp bower`
+- instale o gerador: `npm install -g generator-iask`
+- Execute `yo iask` para criar o projeto.
+- Execute `yo iask:scaffold modulo` para criar um módulo.
+- Execute `gulp build` para construir a versão de produção do projeto.
 
 ## License
 
--- test/general.js
+++ test/general.js
@@ -3,7 +3,7 @@ var path = require('path');
 var helpers = require('yeoman-generator').test;
 var assert = require('yeoman-assert');
 
-describe('general', function() {
+describe('gerador', function() {
   before(function(done) {
     helpers.run(path.join(__dirname, '../generators/app'))
       .inDir(path.join(__dirname, 'temp'))
@@ -19,20 +19,60 @@ describe('general', function() {
       .on('end', done);
   });
 
-  it('the generator can be required without throwing', function() {
+  it('deve ser invocado sem gerar excessões', function() {
     // not testing the actual run of generators yet
     require('../generators/app');
   });
 
-  it('creates expected files', function() {
+  it('deve criar os arquivos esperados', function() {
     assert.file([
-      'bower.json',
-      'package.json',
-      'gulpfile.js',
       '.bowerrc',
+      'bower.json',
       'client',
+      'gulpfile.js',
+      'package.json',
+      'README.md',
       'server',
-      'server/server.js'
+      'client/.jshintrc',
+      'client/app.css',
+      'client/app.js',
+      'client/index.html',
+      'client/plugins.js',
+      'client/static',
+      'client/style.css',
+      'client/static/font',
+      'client/static/images',
+      'client/static/font/material-design-icons',
+      'client/static/font/roboto',
+      'client/static/font/material-design-icons/Material-Design-Icons.svg',
+      'client/static/font/material-design-icons/Material-Design-Icons.ttf',
+      'client/static/font/material-design-icons/Material-Design-Icons.woff',
+      'client/static/font/material-design-icons/Material-Design-Iconsd41d.eot',
+      'client/static/font/roboto/Roboto-Bold.ttf',
+      'client/static/font/roboto/Roboto-Bold.woff',
+      'client/static/font/roboto/Roboto-Light.ttf',
+      'client/static/font/roboto/Roboto-Light.woff',
+      'client/static/font/roboto/Roboto-Medium.ttf',
+      'client/static/font/roboto/Roboto-Medium.woff',
+      'client/static/font/roboto/Roboto-Regular.ttf',
+      'client/static/font/roboto/Roboto-Regular.woff',
+      'client/static/font/roboto/Roboto-Thin.ttf',
+      'client/static/font/roboto/Roboto-Thin.woff',
+      'client/static/images/avatar.png',
+      'client/static/images/favicon',
+      'client/static/images/login-logo.png',
+      'client/static/images/materialize-logo.png',
+      'client/static/images/user-bg.jpg',
+      'client/static/images/favicon/apple-touch-icon-152x152.png',
+      'client/static/images/favicon/favicon-32x32.png',
+      'client/static/images/favicon/mstile-144x144.png',
+      'server/app',
+      'server/config',
+      'server/server.js',
+      'server/app/services',
+      'server/app/services/auth.js',
+      'server/config/database.js',
+      'server/config/express.js'
     ]);
   });
 });
