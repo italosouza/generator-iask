54d5d0ed3585f8881221ccba7db5f459f2a9481f
Italo Souza <italoasouza@gmail.com>
2019-05-15 18:03:12 -0300

    refatoração do gerador - WIP

--- README.md
+++ README.md
@@ -1,14 +1,10 @@
 # IASK [![Build Status](https://travis-ci.org/italosouza/generator-iask.svg?branch=master)](https://travis-ci.org/italosouza/generator-iask) [![Coverage Status](https://coveralls.io/repos/github/italosouza/generator-iask/badge.svg?branch=master)](https://coveralls.io/github/italosouza/generator-iask?branch=master)
 
-> [Yeoman](http://yeoman.io) generator para prototipação de aplicações web utilizando [gulp](http://gulpjs.com/) como ferramenta de construção.
+> [Yeoman](http://yeoman.io) generator para prototipação de aplicações.
 
 ## Passo a passo
 
-- Instale as dependências: `npm install -g yo gulp`
-- instale o gerador: `npm install -g generator-iask`
-- Execute `yo iask` para criar o projeto.
-- Execute `yo iask:scaffold modulo` para criar um módulo.
-- Execute `gulp build` para construir o projeto.
+- Em breve nova descrição de como proceder com a utilização deste gerador.
 
 ## License
 
--- generators/app/index.js
+++ generators/app/index.js
@@ -1,6 +1,6 @@
 'use strict';
 var path = require('path');
-var generators = require('yeoman-generator');
+var Generator = require('yeoman-generator');
 var _ = require('lodash');
 var extend = require('deep-extend');
 var mkdirp = require('mkdirp');
@@ -8,98 +8,132 @@ var yosay = require('yosay');
 
 //valida o nome projeto
 function gerarNomeProjeto(pNome) {
-  pNome = _.kebabCase(pNome);
-  return pNome;
+    pNome = _.kebabCase(pNome);
+    return pNome;
 }
 
-module.exports = generators.Base.extend({
+module.exports = class extends Generator {
 
-  //inicializando configurações do gerador
-  initializing: function() {
-    this.props = {};
-  },
-
-  //solicitando informações sobre o conteudo a ser gerado
-  prompting: function() {
-    var done = this.async();
-
-    var prompts = [{
-      type: 'input',
-      name: 'name',
-      default: gerarNomeProjeto('iask-express'),
-      filter: gerarNomeProjeto,
-      message: 'Informe o nome do projeto:'
-    }];
-
-    this.prompt(prompts, function(pRespostas) {
-      this.props.name = pRespostas.name;
-
-      done();
-    }.bind(this));
-
-  },
-
-  //roda o gerador principal
-  default: function() {
-    //verifica se a pasta atual possui o mesmo nome informado no parametro
-    if (path.basename(this.destinationPath()) !== this.props.name) {
-      this.log(
-        '\nPor padrão o gerador deve ser invocado apartir próprio diretório: ' + this.props.name + '\n' +
-        'Neste caso, iremos criar esta pasta para você.\n'
-      );
-      mkdirp(this.props.name);
-      this.destinationRoot(this.destinationPath(this.props.name));
+    constructor(args, opts) {
+        super(args, opts)
+        this.props = {};
     };
 
-    this.fs.copyTpl(
-      this.templatePath('*.js'),
-      this.destinationPath(path.join('./'))
-    );
-    this.fs.copyTpl(
-      this.templatePath('*.json'),
-      this.destinationPath(path.join('./'))
-    );
-    this.fs.copy(
-      this.templatePath('.gitignore'),
-      this.destinationPath(path.join('./.gitignore'))
-    );
-    this.fs.copy(
-      this.templatePath('*.md'),
-      this.destinationPath(path.join('./'))
-    );
-    this.fs.copy(
-      this.templatePath('.editorconfig'),
-      this.destinationPath(path.join('./.editorconfig'))
-    );
+    /**
+     * Your initialization methods (checking current project state, getting configs, etc)
+     */
+    initializing() {
+        this.log('initializing')
 
-    //define o arquivo template (corresponde a pasta template contida na pasta autal [app])
-    var pkg = this.fs.readJSON(this.templatePath('package.json'), {});
-    extend(pkg, {
-      name: this.props.name
-    });
-    this.fs.writeJSON(this.destinationPath('package.json'), pkg);
+    }
 
-    //gerador do sub-modulo
-    this.composeWith('config', {
-      arguments: ['core']
-    }, {
-      local: require.resolve('../config')
-    });
+    /**
+     * Where you prompt users for options (where you’d call this.prompt())
+     */
+    async prompting() {
 
-  },
+        const answers = await this.prompt([{
+                type: 'input',
+                name: 'name',
+                default: this.appname, // gerarNomeProjeto('iask-express'),
+                filter: gerarNomeProjeto,
+                message: 'Informe o nome do projeto:'
+            },
+            {
+                type: "confirm",
+                name: "cool",
+                message: "Would you like to enable the Cool feature?"
+            }
+        ]);
 
-  //grava as informações no disco
-  writing: function() {},
+        this.log(answers)
 
-  install: function() {
-    // this.installDependencies();
-  },
+        // this.prompt(prompts, function(pRespostas) {
+        //   this.props.name = pRespostas.name;
 
-  end: function() {
+        //   done();
+        // }
 
-    this.log(yosay(`
+    };
+
+    /**
+     * Saving configurations and configure the project (creating .editorconfig files and other metadata files)
+     */
+    configuring() {
+
+    }
+
+    /**
+     * If the method name doesn’t match a priority, it will be pushed to this group.
+     */
+    default () {
+        //verifica se a pasta atual possui o mesmo nome informado no parametro
+        if (path.basename(this.destinationPath()) !== this.props.name) {
+            this.log(
+                '\nPor padrão o gerador deve ser invocado apartir próprio diretório: ' + this.props.name + '\n' +
+                'Neste caso, iremos criar esta pasta para você.\n'
+            );
+            mkdirp(this.props.name);
+            this.destinationRoot(this.destinationPath(this.props.name));
+        };
+
+        this.fs.copyTpl(
+            this.templatePath('*.js'),
+            this.destinationPath(path.join('./'))
+        );
+        this.fs.copyTpl(
+            this.templatePath('*.json'),
+            this.destinationPath(path.join('./'))
+        );
+        this.fs.copy(
+            this.templatePath('.gitignore'),
+            this.destinationPath(path.join('./.gitignore'))
+        );
+        this.fs.copy(
+            this.templatePath('*.md'),
+            this.destinationPath(path.join('./'))
+        );
+        this.fs.copy(
+            this.templatePath('.editorconfig'),
+            this.destinationPath(path.join('./.editorconfig'))
+        );
+
+        //define o arquivo template (corresponde a pasta template contida na pasta autal [app])
+        var pkg = this.fs.readJSON(this.templatePath('package.json'), {});
+        extend(pkg, {
+            name: this.props.name
+        });
+        this.fs.writeJSON(this.destinationPath('package.json'), pkg);
+
+        //gerador do sub-modulo
+        this.composeWith('config', {
+            arguments: ['core']
+        }, {
+            local: require.resolve('../config')
+        });
+
+    };
+
+    /**
+     * Where you write the generator specific files (routes, controllers, etc)
+     */
+    writing() {};
+
+    /**
+     * Where installations are run (npm, bower)
+     */
+    install() {
+        // this.installDependencies();
+    };
+
+    /**
+     *  Called last, cleanup, say good bye, etc
+     */
+    end() {
+
+        this.log(yosay(`
     \nAtenção às dependencias globais:\nnpm install -g @angular/cli gulp
     \nExperimente os comandos: \nyo iask:scaffold COMPONENTE\nnpm run build`, { maxLength: 50 }));
-  }
+    }
 
-});
+};
